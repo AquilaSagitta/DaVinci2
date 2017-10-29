7eb57b42700f02fd7bc99f42952bf4de9390c602
Chris Blahut <c.blahut@foreverbride.com>
2017-10-28 23:13:26 -0500

    Added PHPUnit

---
+++ phpunit.bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mia
+ * Date: 10/28/17
+ * Time: 10:41 PM
+ */
+
+require "./vendor/autoload.php";
---
+++ src/sanityTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mia
+ * Date: 10/28/17
+ * Time: 11:10 PM
+ */
+
+namespace DaVinci;
+
+
+class sanityTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSanity() {
+        static::assertTrue(true);
+    }
+}
